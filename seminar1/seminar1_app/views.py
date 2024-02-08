60db7132f63093c9b2fe338b1893d8590de66d1f
bloodyblade <freesxsad23@mail.ru>
2024-02-09 01:38:11 +0300

    added views for routes and logger decorator

--- seminar1/seminar1_app/views.py
+++ seminar1/seminar1_app/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+import logging
 
-# Create your views here.
+logger = logging.getLogger(__name__)
+
+def logger_(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        logger.info(f"{func.__name__} was called")
+        return result
+    return wrapper
+
+@logger_
+def index(request):
+    html = """
+    <h1>Добро пожаловать на мой первый Django-сайт!</h1>
+    <a href="about/">Обо мне</a>
+    """
+    return HttpResponse(html)
+
+@logger_
+def about(request):
+    html = """
+    <h1>Обо мне</h1>
+    <p>Я начинающий веб-разработчик. Я увлекаюсь созданием сайтов и приложений, и этот проект - мой первый опыт в Django.</p>
+    <a href="/">Назад</a>
+    """
+    return HttpResponse(html)
