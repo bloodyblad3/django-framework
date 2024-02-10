f0a598b71af7ba891fbc06b1acbaeba99d955723
bloodyblade <freesxsad23@mail.ru>
2024-02-10 13:06:25 +0300

    added models for db

--- seminar2/seminar2_app/models.py
+++ seminar2/seminar2_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Client(models.Model):
+    name = models.CharField()
+    email = models.EmailField()
+    phone_number = models.CharField()
+    adress = models.CharField()
+    sign_up_date = models.DateField(auto_now_add=True)
+
+class Product(models.Model):
+    name = models.CharField()
+    description = models.CharField()
+    price = models.DecimalField(decimal_places=2)
+    quantity = models.IntegerField()
+    addition_date = models.DateField(auto_now_add=True)
+
+class Order(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+    order_sum = models.DecimalField(decimal_places=2)
+    order_date = models.DateField(auto_now_add=True)
