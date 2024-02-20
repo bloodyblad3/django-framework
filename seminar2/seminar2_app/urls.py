cc149108855448f4b54e4728f069c1ec59d993ac
bloodyblade <freesxsad23@mail.ru>
2024-02-20 11:39:00 +0300

    added route for upload image

--- seminar2/seminar2_app/urls.py
+++ seminar2/seminar2_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("orders/<int:client_id>/", views.get_orders, name="orders")
+    path("orders/<int:client_id>/", views.get_orders, name="orders"),
+    path('upload/', views.upload_image, name='upload_image'),
 ]
